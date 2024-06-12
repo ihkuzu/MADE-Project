37a31c94f864d9798accf82a594297aeb3026d5f
ihkuzu <76225179+ihkuzu@users.noreply.github.com>
2024-06-13 01:12:29 +0200

    Updated test.py

--- project/test.py
+++ project/test.py
@@ -7,37 +7,37 @@ class TestPipeline(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Set up the test environment before running any tests."""
+        """Building test enviroment"""
         process_data()
         cls.db_path = './data/nyc_climate_traffic.db'
         cls.conn = sqlite3.connect(cls.db_path)
         cls.cursor = cls.conn.cursor()
 
-    def test_database_creation(self):
-        """Test if the database file is created."""
+    def testing_database_created(self):
+        """Testing if the database created or not"""
         self.assertTrue(os.path.exists(self.db_path), "Database file was not created.")
 
-    def test_table_existence(self):
-        """Test if the necessary tables exist."""
+    def testing_table_exist(self):
+        """Testing if the necessary tables exist or not"""
         tables = ['traffic_accidents', 'weather_data']
         for table in tables:
             with self.subTest(table=table):
                 self.cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';")
                 self.assertIsNotNone(self.cursor.fetchone(), f"Table '{table}' was not created in the database.")
 
-    def test_traffic_accidents_data(self):
-        """Test that the traffic_accidents table contains data."""
+    def testing_traffic_accidents_datatable(self):
+        """Testing to traffic_accidents table contains data"""
         self.cursor.execute("SELECT COUNT(*) FROM traffic_accidents;")
         self.assertGreater(self.cursor.fetchone()[0], 0, "Traffic accidents table does not contain any data.")
 
-    def test_weather_data(self):
-        """Test that the weather_data table contains data."""
+    def testing_weather_datatable(self):
+        """Testing to weather_data table contains data"""
         self.cursor.execute("SELECT COUNT(*) FROM weather_data;")
         self.assertGreater(self.cursor.fetchone()[0], 0, "Weather data table does not contain any data.")
 
     @classmethod
     def tearDownClass(cls):
-        """Clean up after all tests."""
+        """Clean up after all tests"""
         cls.conn.close()
         if os.path.exists(cls.db_path):
             os.remove(cls.db_path)
