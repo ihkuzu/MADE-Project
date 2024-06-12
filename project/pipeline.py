4ae0e5ae37abd8fe6b2c271baff2b5dc65a65e06
ihkuzu <76225179+ihkuzu@users.noreply.github.com>
2024-06-12 23:21:18 +0200

    Updated pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -4,8 +4,8 @@ import zipfile
 import pandas as pd
 import sqlite3
 
-traffic_accidents_url = 'https://storage.googleapis.com/kaggle-data-sets/1581366/2602130/compressed/NYC%20Accidents%202020.csv.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240522%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240522T211503Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=39dcaac3ff083194cf999143a6d02cc017e5e499d6211f1279dac962cd88606f9c88cd1fa0806e5b995e9f6be8bbe05e18effcd9e0fae4b4f5bfa1c3b8ce1899c83d69d2c3744cf029231eb732620d4da41f9392e640f6dcc0ad147b35857c949f8575ccad52b81080ebee64bafd75540bb69df6e84ff2d23d8f55a4b0d120123227f5f3cb5fb904ac90468ab79f1575526daca91f679166abbb39a27c46267145992393442f4aacfe6dc102297a0dd04f75dda6b386c6940975c8aa3d5692b118f5a8315d2208cbb678264f954b3bd36ff3542a51b7c88cde289360ac48e58adce706ba52687fb1dee8b08305909c1e523e1847835c2ccf76bec52bef575b41'
-weather_data_url = 'https://storage.googleapis.com/kaggle-data-sets/2576340/4389349/compressed/NYC_Weather_2016_2022.csv.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240522%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240522T190336Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=317b6e477585fef9c6b23deec3186513dd1d9f7644bc31844267658738eca62223de77a0f37431384fc778fe7c0fd84b9e722baed9047b9ed466965f7a6fc0c364cf137e11f7e13766e0e5fa6eac5a8ca5982096dfedb38394304750f85b04443998fb05af44b15bae1a6a986b5c50fc76b98c11998f4b4141e05f56d89b6a29874f8859eff403e087499165c4040937cd40db2677058c67e8b085e3f292c1a35699ecf588689589f5109b12f0af1ee3f40a3d9c472313d11dd486a65efe31c74199d555d9c40d7aba1ed1a1c93770e43ee9eb4806202fe0adfbcaf94a0bb686075c6ae1df67111509b90d5505f574e93cfd46259b732557562e974a6fa646d4'
+traffic_accidents_url = 'https://storage.googleapis.com/kaggle-data-sets/1581366/2602130/compressed/NYC%20Accidents%202020.csv.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240612%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240612T211758Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=0d925610ff4efd7f899ec48c61ae12d0542ff21c977f158c782974ecde886a01235a8d3e6fdd0ab5e892cae6e59863c290595d719db775d71254fced349476dde7a9e7d8fcc2a44252396119cb716cef555ec30262167d9305bfdf90abe420111294ca72475e78ca9499b43a8c2c1ac6517d7ba36397873f4584cab0191f1812d5b56b3a756354be24e0bae45af0f07709eb1d2335aef7ea96976696a9767a339039dcc8c20fb39c6dfe26778b7d884de569ee6bf57feb6000c37bb2b5aeb12ade3782ba9f29206e8446b8f131dcad7bafb669eeb49b3b41e53a401672f74bc1d9232374f0a9170fb12b653165ccd2914f4f16180ff0b777b476643285f9e89b'
+weather_data_url = 'https://storage.googleapis.com/kaggle-data-sets/2576340/4389349/compressed/NYC_Weather_2016_2022.csv.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240612%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240612T211931Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=b79f0a9c907e8bc7858db51cc8b8e449b59a19f77a165846356daf7337915079ff0fcac70f095b23807899fc2bed28bdc10655e684699a30a0ecff071c8d9f9d6337fda0f9f51752e75f1f4542aeade370dff2f035b3da4ba2a5e5caa6c54690b3b5d9b79cef6010b93ab8b3f2a399edc35a4459de8b7bfe7a09e0afa7a0eed6ae38ffd1ff636e2adb8ae91662af2747665a2764eea5bdaa4bbe5f5518681f21bf9d441ec8506559940d7ebe3de74536db202bda8c35c006dd60048deabdc02c46007157250140d5f4f7ef2de9b3b9d68214599f8206ac61f1ce803631362ea2e175bbe3bf1ea8e723ed925b600c76cadc94093d39e9995eb754839d0df8f55e'
 
 def download_data(url, filename):
     headers = {
@@ -42,10 +42,14 @@ def process_data():
     accidents_df = pd.read_csv(traffic_csv_path)
     weather_df = pd.read_csv(weather_csv_path)
 
+    # Filter weather data for the date range 01/2020 - 08/2020
+    weather_df['time'] = pd.to_datetime(weather_df['time'])
+    weather_df = weather_df[(weather_df['time'] >= '2020-01-01') & (weather_df['time'] < '2020-09-01')]
+
     accidents_df.fillna(0, inplace=True)
     weather_df.fillna(0, inplace=True)
 
-    conn = sqlite3.connect(os.path.join(data_dir, 'nyc_climate_traffic.db'))
+    conn = sqlite3.connect(os.path.join(data_dir, 'nyc_climate_traffic1.db'))
     accidents_df.to_sql('traffic_accidents', conn, if_exists='replace', index=False)
     weather_df.to_sql('weather_data', conn, if_exists='replace', index=False)
     conn.close()
